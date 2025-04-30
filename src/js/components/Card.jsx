ab87e76161d0eadbf27149e820e6e9953a9f5992
Felipe Moreno <85708817+blacklottus-09@users.noreply.github.com>
2025-04-30 22:58:32 +0000

    the title and button of the component card was center

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -5,10 +5,10 @@ const Card = () => {
         <div className="row">
             <div className="card p-3">
                 <img src="https://picsum.photos/500/325" className="card-img-top" alt="Random image from lorem picsum"/>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
+                <div className="card-body text-center">
+                    <h5 className="card-title ">Card title</h5>
                     <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-                    <a href="#" className="btn btn-primary">Find out more</a>
+                    <a href="#" className="btn btn-primary ">Find out more</a>
                 </div>
             </div>
         </div>
